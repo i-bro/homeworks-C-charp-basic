7b1b31f0abd2ba155bff9e69ad00e0472417e068
i-bro <ibrogp2@gmail.com>
2026-03-22 00:11:40 +0100

    homework 2 done

---
+++ HomeWork2/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Enter integer no.{i + 1}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int num in numbers)
+{
+    if (num % 2 == 0)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine($"The result is: {sum}");
---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Ana", "Marija", "Elena", "Sara", "Ivana" };
+
+Console.Write("Enter student group (1 or 2): ");
+int group = int.Parse(Console.ReadLine());
+
+if (group == 1)
+{
+    Console.WriteLine("The Students in G1 are:");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (group == 2)
+{
+    Console.WriteLine("The Students in G2 are:");
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter 1 or 2.");
+}
