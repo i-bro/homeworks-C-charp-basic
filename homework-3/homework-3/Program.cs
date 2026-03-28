bf5f3ca4ad024a6eecf4810891825f8825d9413c
i-bro <ibrogp2@gmail.com>
2026-03-28 01:03:49 +0100

    homework 3 done

---
+++ homework-3/homework-3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("global age calculator");
+
+Console.Write("enter your birth date(yyyy-mm-dd):");
+string dateInput = Console.ReadLine();
+
+if (DateTime.TryParse(dateInput, out DateTime birthDate))
+{
+    int age = AgeCalculator(birthDate);
+
+    Console.WriteLine($"based on universal time (utc) you are {age} years old");
+} else
+{
+    Console.WriteLine("invalid date format");
+}
+
+static int AgeCalculator(DateTime birthDate)
+{
+    DateTime today = DateTime.UtcNow.Date;
+    int age = today.Year - birthDate.Year;
+
+    if(birthDate.AddYears(age) > today)
+    {
+        age--;
+    }
+    return age;
+}
