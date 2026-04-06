25b6688ec0a65839c116f96eaab6387ba4fe8d4c
i-bro <ibrogp2@gmail.com>
2026-04-06 02:50:31 +0200

    homework 8 done

---
+++ homework08/homework08/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Queue<int> numbersQueue = new Queue<int>();
+bool keepAdding = true;
+
+while (keepAdding)
+{
+    Console.WriteLine("Enter a number");
+    if(int.TryParse(Console.ReadLine(), out int result))
+    {
+        numbersQueue.Enqueue(result);
+    }
+    else
+    {
+        Console.WriteLine("That wasn't a valid number");
+    }
+    Console.Write("Do you want to enter anouther number> Y/N :");
+    string response = Console.ReadLine().ToUpper();
+
+    if (response != "Y")
+    {
+        keepAdding = false;
+    }
+}
+
+
+while(numbersQueue.Count > 0)
+{
+    int currentNumber = numbersQueue.Dequeue();
+    Console.WriteLine(currentNumber);
+}
